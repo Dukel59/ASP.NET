1ec1eb659e4df9807b6946e2c0ebaf58d23e4570
Dukel59 <jonnydukalez@gmail.com>
2021-09-09 23:23:19 +0300

    Create ViewModels

---
+++ Homework_1/HM.BLL/ViewModels/MediaFiles/CreateMediaFiles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HM.BLL.ViewModels.MediaFiles
+{
+    public class CreateMediaFiles
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Path { get; set; }
+        public Guid? FeedbackId { get; set; }
+    }
+}
