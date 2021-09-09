ee39b2995809109099e9796d94c9eaa587134e62
Dukel59 <jonnydukalez@gmail.com>
2021-09-09 23:17:45 +0300

    create ICommentService and VMs Comment

---
+++ Homework_1/HM.BLL/Interfaces/ICommentService.cs
@@ -0,0 +1,14 @@
+﻿using HM.BLL.ViewModels.Comment;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM.BLL.Interfaces
+{
+   public  interface ICommentService
+    {
+        Task<Guid> CreateCommentAsync(CreateComment comment);
+        List<InfoComment> FindCommentsByFunc(Func<ICommentService, bool> func);
+    }
+}
---
+++ Homework_1/HM.BLL/ViewModels/Comment/CreateComment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HM.BLL.ViewModels.Comment
+{
+    public class CrateComment
+    {
+        public string AuthorName { get; set; }
+        public string Text { get; set; }
+        public Guid FeedbackId { get; set; }
+    }
+}
