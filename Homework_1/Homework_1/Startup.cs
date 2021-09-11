2ca7ffcbfa3af67e88a4547548cb4d9b18e710a6
Dukel59 <jonnydukalez@gmail.com>
2021-09-11 18:10:16 +0300

    Modified Startup.cs

--- Homework_1/Homework_1/Startup.cs
+++ Homework_1/Homework_1/Startup.cs
@@ -2,9 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HM.BLL.Interfaces;
+using HM.BLL.Services;
+using HM.DAL;
+using HM.DAL.Patterns;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +18,23 @@ namespace Homework_1
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
+            services.AddControllers();
+
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            services.AddTransient<ICommentService, CommentService>();
+            services.AddTransient<IFeedbackService, FeedbackService>();
+            services.AddTransient<IMediaFilesService, MediaFilesService>();
+            services.AddTransient<IProductService, ProductService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +45,15 @@ namespace Homework_1
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
